2f5773c42aa5e48fee25a8068c6e89a5a1a85987
izmktr <izmktr@gmail.com>
2025-06-23 22:07:59 +0900

    ポップアップの最小サイズを変更し、オプション表示ボタンを追加しました。

--- popup.js
+++ popup.js
@@ -51,3 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+document.getElementById('show-options').addEventListener('click', () => {
+  chrome.runtime.openOptionsPage();
+});
+
