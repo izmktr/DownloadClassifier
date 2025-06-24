cd4bf4bf27606ff21b4b3b5ea0985471ec0e0222
izmktr <izmktr@gmail.com>
2025-06-24 22:33:21 +0900

    ダウンロードファイル名の決定時にルールを取得し、マッチした場合は指定フォルダに保存する機能を実装しました。デバッグログの送信機能も追加しました。

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 // background.js
+importScripts('downloadRule.js');
+
 chrome.runtime.onInstalled.addListener(() => {
   console.log('DownloadClassifier 拡張機能がインストールされました');
 });
@@ -6,3 +8,36 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.downloads.onCreated.addListener((downloadItem) => {
   console.log('新しいダウンロード:', downloadItem);
 });
+
+function getRules(callback) {
+  chrome.storage.sync.get(['rules'], (result) => {
+    const rules = (result.rules || []).map(ruleObj => new DownloadRule(ruleObj));
+    callback(rules);
+  });
+}
+
+function sendDebugLog(msg) {
+  chrome.runtime.sendMessage({ type: 'debug-log', text: msg });
+}
+
+chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
+  getRules((rules) => {
+    const matched = rules.find(rule => rule.match(item));
+    if (matched && matched.folder) {
+      const filename = item.filename.split(/[\\/]/).pop();
+      suggest({ filename: `${matched.folder}/${filename}` });
+      const logMsg = `rule match: ${item.filename} → ${matched.folder} [${matched.name}]`;
+      console.log(logMsg);
+      sendDebugLog(logMsg);
+      return;
+    } else {
+      suggest();
+      const logMsg = `rule unmatch: ${item.filename}`;
+      console.log(logMsg);
+      sendDebugLog(logMsg);
+      return;
+    }
+  });
+});
+
+
